cb5a6196a92600b7ca92491d1afecf4298be7768
BackBarrel6 <rares.dobrisan@gmail.com>
2024-02-10 17:00:51 +0200

    da

--- src/main/java/bobe/modulet/Food/Mamaligablock.java
+++ src/main/java/bobe/modulet/Food/Mamaligablock.java
@@ -16,11 +16,11 @@ import org.slf4j.LoggerFactory;
 
 public class Mamaligablock extends Block {
 	public static final Logger LOGGER = LoggerFactory.getLogger("modulet");
-	public static final IntProperty QATE = IntProperty.of("qate", 0, 4); // quantity ate
+	public static final IntProperty QATE = IntProperty.of("qate", 0, 6); // quantity ate
 
 	public Mamaligablock(AbstractBlock.Settings settings) {
 		super(settings);
-		setDefaultState(getDefaultState().with(QATE, 4));
+		setDefaultState(getDefaultState().with(QATE, 6));
 	}
 
 	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
@@ -28,24 +28,22 @@ public class Mamaligablock extends Block {
 	}
 
 	@Override
-	public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+	public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand,
+			BlockHitResult hit) {
 		if (!world.isClient()) {
-			int newQateValue = state.get(QATE) - 1;
-
-			if (newQateValue >= 0) {
+			int newQateValue = state.get(QATE);
+			if (newQateValue == 0) {
+				world.removeBlock(pos, true);
+				player.heal(6F);
+			} else {
+				world.setBlockState(pos, state.with(QATE, newQateValue - 1));
+			}
+			if (newQateValue > 1) {
 				player.heal(6F);
 				LOGGER.info("Used mamaliga");
-
-				if (newQateValue == 0) {
-					world.removeBlock(pos, true);
-				} else {
-					world.setBlockState(pos, state.with(QATE, newQateValue));
-				}
-
 				return ActionResult.SUCCESS;
 			}
 		}
-
 		return ActionResult.SUCCESS;
 	}
 }
--- src/main/java/bobe/modulet/Modulet.java
+++ src/main/java/bobe/modulet/Modulet.java
@@ -1,7 +1,6 @@
 package bobe.modulet;
 
-import bobe.modulet.Food.Mamaliga;
-import bobe.modulet.Food.Mamaligablock;
+import bobe.modulet.Food.*;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
@@ -16,13 +15,13 @@ import org.slf4j.LoggerFactory;
 public class Modulet implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("modulet");
 	public static final Mamaliga mamaliga = new Mamaliga(new FabricItemSettings());
-	public static final Block mamaligablock = new Mamaligablock(FabricBlockSettings.create().strength(1f));
-	//TODO:adaugat retete pentru toate itemele :D(blocul de mamaliga se face din 4 mamaliga
+	public static final Block mamaliga_block = new Mamaligablock(FabricBlockSettings.create().strength(1f));
+	//TODO:adaugat retete pentru toate itemele :D(blocul de mamaliga se face din 4 mamaliga)
 	@Override
 	public void onInitialize() {
 		Registry.register(Registries.ITEM, new Identifier("modulet", "mamaliga"), mamaliga);
-		Registry.register(Registries.BLOCK, new Identifier("modulet", "mamaligablock"), mamaligablock);
-		Registry.register(Registries.ITEM, new Identifier("modulet", "mamaligablock"), new BlockItem(mamaligablock, new FabricItemSettings()));
+		Registry.register(Registries.BLOCK, new Identifier("modulet", "mamaliga_block"), mamaliga_block);
+		Registry.register(Registries.ITEM, new Identifier("modulet", "mamaliga_block"), new BlockItem(mamaliga_block, new FabricItemSettings()));
 
 		LOGGER.info("Hello Fabric world!");
 	}
